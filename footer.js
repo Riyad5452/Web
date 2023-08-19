17fe1e4fd8d6bbe0d0ae30bad7a8532e3ad123be
Riyad5452 <98330148+Riyad5452@users.noreply.github.com>
2023-08-19 21:31:02 +0800

    Delete footer.js

--- footer.js
+++
@@ -1,4 +0,0 @@
-function setconfig(){
-      const dd = new Date()
-      document.querySelector('.copyleft').innerHTML = "Copyright  <img src='https://mirrors.creativecommons.org/presskit/icons/heart.red.png' width='25' height='25' alt='cc'> @DDDD Created by <a href='https://xxxx.blogspot.com' target='_blank'>xxxx.</a>"
-    }
