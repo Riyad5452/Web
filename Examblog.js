3e267e45d5660581dfe7c66927d18d33fb46ef58
Riyad5452 <98330148+Riyad5452@users.noreply.github.com>
2023-08-19 21:29:56 +0800

    Delete Examblog.js

--- Examblog.js
+++
@@ -1,4 +0,0 @@
-function setting(){
-       document.querySelector('.copyright').innerHTML = "Copyright  @ Examblog Created by <a href="https://www.facebook.com/groups/exambloggas"
-        target="_blank"><i class="fa-brands fa-facebook fa-2x"></i></a>"
-    }
