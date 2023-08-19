5d024fc9159a7a4ddfcdf63fae24242a18abc72b
Riyad5452 <98330148+Riyad5452@users.noreply.github.com>
2023-08-20 03:19:29 +0800

    Delete Medi35.js

--- Medi35.js
+++
@@ -1,4 +0,0 @@
-function setting(){
-       document.querySelector('.copyright').innerHTML = "Copyright  @ Mediblog Created by <a href="https://www.facebook.com/groups/Medibloggas"
-        target="_blank"><i class="fa-brands fa-facebook fa-2x"></i></a>"
-    }
