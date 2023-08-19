61e4414b4f9f9c66369577f1054d5eb23b0c8728
Riyad5452 <98330148+Riyad5452@users.noreply.github.com>
2023-08-19 21:58:19 +0800

    Add files via upload

---
+++ Medi35.js
@@ -0,0 +1,4 @@
+function setting(){
+       document.querySelector('.copyright').innerHTML = "Copyright  @ Mediblog Created by <a href="https://www.facebook.com/groups/Medibloggas"
+        target="_blank"><i class="fa-brands fa-facebook fa-2x"></i></a>"
+    }
