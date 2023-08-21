bc0ff69cb20675daabb3dfc84b45b58536b10411
Riyad5452 <98330148+Riyad5452@users.noreply.github.com>
2023-08-22 00:40:50 +0800

    Update FtMediblog.js

--- js/FtMediblog.js
+++ js/FtMediblog.js
@@ -1,19 +1,4 @@
 function setFooter(){
-  document.querySelector('.copyright').innerHTML = '<center>
-            <div class="card-social">
-            <a href="https://www.linkedin.com/in/riyad-hossain-820852193" class="linkedin">
-            <i class="fa-brands fa-linkedin"></i>
-            </a>
-
-            <a href="https://www.facebook.com/profile.php?id=100009202179584/" target="_blank" class="facebook">
-            <i class="fab fa-facebook"></i>
-            </a>
-            <a href="https://wa.me/+01125995306" target="_blank" class="whatsapp">
-            <i class="fab fa-whatsapp"></i>
-            </a>
-            <a href="https://www.instagram.com/rhriyadhossain/" class="instagram">
-            <i class="fab fa-instagram"></i>
-            </a>
-            </div>
-          </center>'
+  document.querySelector('.copyright').innerHTML = '<center><div class="card-social"><a href="https://www.linkedin.com/in/riyad-hossain-820852193" class="linkedin"><i class="fa-brands fa-linkedin"></i></a><a href="https://www.facebook.com/profile.php?id=100009202179584/" target="_blank" class="facebook">
+            <i class="fab fa-facebook"></i></a><a href="https://wa.me/+01125995306" target="_blank" class="whatsapp"><i class="fab fa-whatsapp"></i></a><a href="https://www.instagram.com/rhriyadhossain/" class="instagram"><i class="fab fa-instagram"></i></a></div></center>'
   }
